59a02df485c939da6a2bdd1e7721f15bed341d8e
Your Name <you@example.com>
2025-11-17 21:44:15 -0300

    feat(ProdutoRepository): adicionei método findByNome para validar nomes duplicados

--- src/main/java/sistemas/compras/repository/ProdutoRepository.java
+++ src/main/java/sistemas/compras/repository/ProdutoRepository.java
@@ -1,7 +1,12 @@
 package sistemas.compras.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import sistemas.compras.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    // Usado no Service para verificar se já existe um produto com o mesmo nome
+    Optional<Produto> findByNome(String nome);
 }
