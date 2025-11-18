15c2c8b1e7fe2a4fe33c707b5d68933688a5c6dd
Your Name <you@example.com>
2025-11-17 21:42:32 -0300

    feat(CategoriaService): criei o service e movi a lógica do controller para ele

--- src/main/java/sistemas/compras/controller/CategoriaController.java
+++ src/main/java/sistemas/compras/controller/CategoriaController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import sistemas.compras.model.Categoria;
-import sistemas.compras.repository.CategoriaRepository;
+import sistemas.compras.service.CategoriaService;
 
 @Controller
 public class CategoriaController {
 
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    private CategoriaService categoriaService;
 
     @GetMapping("/")
     public String home() {
@@ -23,7 +23,7 @@ public class CategoriaController {
 
     @GetMapping("/categoria")
     public String listarCategorias(Model model) {
-        model.addAttribute("categorias", categoriaRepository.findAll());
+        model.addAttribute("categorias", categoriaService.listarTodas());
         return "categoria";
     }
 
@@ -35,30 +35,26 @@ public class CategoriaController {
 
     @PostMapping("/categoria/salvar")
     public String salvarCategoria(Categoria categoria) {
-        categoriaRepository.save(categoria);
+        categoriaService.salvar(categoria);
         return "redirect:/categoria";
     }
-    // Método para excluir uma categoria pelo ID
+
     @GetMapping("/categoria/excluir/{id}")
     public String excluirCategoria(@PathVariable Long id) {
-    categoriaRepository.deleteById(id); // Apaga do banco
-    return "redirect:/categoria"; // Redireciona para a listagem correta
+        categoriaService.excluir(id);
+        return "redirect:/categoria";
     }
-// Método para exibir o formulário de edição
+
     @GetMapping("/categoria/editar/{id}")
     public String editarCategoriaForm(@PathVariable Long id, Model model) {
-        Categoria categoria = categoriaRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("ID de categoria inválido: " + id));
-
-    model.addAttribute("categoria", categoria);
-    return "formCategoria"; // usa o mesmo formulário de cadastro
+        Categoria categoria = categoriaService.buscarPorId(id);
+        model.addAttribute("categoria", categoria);
+        return "formCategoria";
     }
 
-// Método para atualizar a categoria (usa o mesmo /categoria/salvar)
     @PostMapping("/categoria/atualizar")
     public String atualizarCategoria(Categoria categoria) {
-        categoriaRepository.save(categoria); // o save atualiza se já existir ID
+        categoriaService.salvar(categoria);
         return "redirect:/categoria";
-}
-
+    }
 }
